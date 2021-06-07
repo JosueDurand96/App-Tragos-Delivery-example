e94a5732902142b2b959dd1fa2cd5c0658b26cc5
JosueDurand96 <josue.durand8@gmail.com>
2021-06-07 03:16:45 -0500

    navigation fragments

--- app/src/main/java/com/durand/apptragos/ui/MainFragment.kt
+++ app/src/main/java/com/durand/apptragos/ui/MainFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.durand.apptragos.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -22,5 +24,12 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-  
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        detailButton.setOnClickListener {
+            findNavController().navigate(R.id.detailTragoFragment)
+        }
+
+    }
 }
